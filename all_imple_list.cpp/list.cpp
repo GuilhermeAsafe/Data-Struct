15f95e3e7d3dc50e383c376f4ca25f74e5883c44
GuilhermeAsafe <g.asafe.ufmg@gmail.com>
2025-07-29 19:28:58 -0300

    add: sorts

---
+++ all_imple_list.cpp/list.cpp
@@ -0,0 +1,26 @@
+#include <list.hpp>
+
+
+void algorthm::insert_list(int data_){
+
+	node *aux = new node; 
+	
+	aux->next = nullptr;
+	aux->data = data_;
+
+	head = tail;
+
+	
+
+
+
+
+
+}
+
+void algorthm::order_list(){
+
+
+
+
+}
